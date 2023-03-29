bbdff81f6b4e91b8453ca7f915718431240c3a3e
Sther <sthefanypequeno@gmail.com>
2023-03-29 13:18:21 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,11 @@
+//tipo de dado
+//booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+console.log(numero + numeroString)
+
+
+//conversão explícita
---
+++ truthyandFalsy.js
@@ -0,0 +1,25 @@
+//boolean
+
+const usuarioOn=true;
+const contaPaga=false;
+
+//truthy or falsy = NÃO SÃO BOOLEANOS mas podem ser considerados
+
+// 0 ==>false
+// 1 ==> true
+
+
+console.log(0==false);
+console.log(''==false);
+console.log(1 ==true);
+
+
+//null
+//undefined
+
+let minhaVar;
+let varNull = null;
+let numero = 3;
+let texto = "Alura";
+console.log(typeof minhaVar);
+console.log(typeof varNull);
