113d6b55ac950b7ce242decf6f649ef5ee84f86b
Sther <sthefanypequeno@gmail.com>
2023-03-30 13:11:42 -0300

    Finalizando o curso

---
+++ expressao-funcao.js
@@ -0,0 +1,23 @@
+// function minhaFuncao(param){
+// /// PRA NADA.
+// }
+
+// minhaFuncao("param")
+
+// const soma = function(num1, num2){return num1+ num2}
+
+// console.log(soma (1 , 1))
+
+// diferença principal
+
+console.log(apresentar())
+
+function apresentar()
+{
+    return "olá";
+}
+
+console.log(soma(1,1))
+const soma = function(num1, num2) {
+    return num1 + num2;
+}
